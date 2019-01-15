735fcd813bf188b0c987def929f44048290d2d37
saimmehmood <bsef12m557@pucit.edu.pk>
2019-01-15 14:32:35 -0500

    1/15/2019 : 2:32 PM

--- grid.py
+++ grid.py
@@ -13,10 +13,13 @@ def GridTrajectories(grid, lat1, long1, lat2, long2):
     cell_height = height/grid
     cell_width = width/grid
  
-    bearing = calculation.calculateBearing(lat1, long1, lat2, long2)
+ 	# direction from one point towards another one
+    bearing = calculation.calculateBearing(lat1, long1, lat2, long1)
   
-    diagonal = math.pow(cell_height, 2) + math.pow(cell_width, 2)
+    dia = math.pow(cell_height, 2) + math.pow(cell_width, 2)
+    diagonal = math.sqrt(dia)
 
-    cell_digonal_coordinate = calculation.calculateDestination(lat1, long1, bearing, diagonal)
+    cell_digonal_coordinate = calculation.calculateDestination(lat1, long1, bearing, cell_height)
+    print(cell_digonal_coordinate)
 
 GridTrajectories(2, 43.672545, -79.412068, 43.669763, -79.403223)
