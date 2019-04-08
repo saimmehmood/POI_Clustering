71a87a0fd42557c0bb50b1be21e3218aa0e4b0ec
Saim Mehmood <saim.mehmood@hotmail.com>
2019-04-08 13:46:13 -0400

    4/8/2019 1:46 PM

--- embeddings/real_model_graph.py
+++ embeddings/real_model_graph.py
@@ -2,25 +2,26 @@ import networkx as nx
 import pandas as pd
 
 
-df = pd.read_csv('trajascells.csv')
+df = pd.read_csv('check.csv')
 
 traj_id = df['traj_id']
 cell_id = df['cell_id']
-cell_names = df['cell_names']
 
 graph = nx.Graph()
 
-for i in range(len(cell_names)):
-	graph.add_node(cell_names[i])
+for i in range(len(cell_id)):
+	graph.add_node(cell_id[i])
 
-ids = {}
+output = []
 
-for i in range(len(traj_id)):
+for x in traj_id:
+    if x not in output:
+        output.append(x)
 
+for i in range(len(output)):
+	
 	for j in range(len(traj_id)):
+		
+		if(output[i] == traj_id[j]):
 
-		if(traj_id[i] == traj_id[j]):
-			ids['traj_id[i]'] = {cell_names[j]}
-
-
-print(ids)
+			print(traj_id[j], cell_id[j])
