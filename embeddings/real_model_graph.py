11f9294e2b84b125fe14f2a0244edfbd4d8ff330
Saim Mehmood <saim.mehmood@hotmail.com>
2019-04-08 18:52:46 -0400

    4/8/2019 6:52 PM

--- embeddings/real_model_graph.py
+++ embeddings/real_model_graph.py
@@ -1,7 +1,6 @@
 import networkx as nx
 import pandas as pd
 
-
 df = pd.read_csv('check.csv')
 
 traj_id = df['traj_id']
@@ -15,17 +14,16 @@ for i in range(len(cell_id)):
 output = []
 
 for x in traj_id:
-    if x not in output:
-        output.append(x)
+	if x not in output:
+		output.append(x)
 
 temp = []
 
 for i in range(len(output)):
-	
-	for j in range(len(traj_id)):
-		
-		if(output[i] == traj_id[j]):
 
+	for j in range(len(traj_id)):
+
+		if (output[i] == traj_id[j]):
 			temp.append(str(traj_id[j]) + "," + str(cell_id[j]))
 
 s1 = []
@@ -39,10 +37,22 @@ for tmp in temp:
 st_edge = []
 
 for i in range(len(s1)):
+	# print(s1[i], s2[i])
+	try:
+		if (s1[i] == s1[i + 1]):
+			graph.add_edge(s2[i], s2[i + 1])
+	except IndexError:
+		print("")
 
-	if (s1[i + 1] == s1[i]):
-		st_edge.append(s2[i])
+# print(graph.edges())
+list_of_edges = list(graph.edges)
+
+nodes = []
+for i in range(len(list_of_edges)):
+	nodes.append(str(list_of_edges[i]).replace("(", "").replace(")", "").replace(", ", " ").replace("'", ""))
 
 
+f = open("realm_nodes.edgelist", "w")
 
-	
+for i in range(len(nodes)):
+	f.write(nodes[i] + "\n")
