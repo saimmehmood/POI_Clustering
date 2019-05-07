6efb98becfa3cc8af0077b1f0411920041c15798
saimmehmood <bsef12m557@pucit.edu.pk>
2019-05-07 19:09:52 -0400

    5/7/2019 7:09 PM

--- embeddings/getting_vectors_cosine_sim.py
+++ embeddings/getting_vectors_cosine_sim.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+from pprint import pprint
 
 
 def cos_sim(a, b):
@@ -63,25 +64,31 @@ def getting_vector_cosine_sim(vector_file_01, vector_file_02):
     i = int(min_label)
     m = int(max_label)
 
-    #print(null_dict)
-    # if '3573' in null_dict:
-    #print(null_dict[str(i)])
+
     f_cos_sim = open("cos_sim.csv", "w")
     f_cos_sim.write("node1,node2,null_cos_sim,real_cos_sim,diff\n")
 
+
+
     start = time.time()
+
+
+    # two while loops only get n*(n-1) / 2 pairs out of cell nodes.
     while i < m:
 
         j = i + 1
 
-        while i < j <= m:
+        while j <= m:
 
-            f_cos_sim.write(str(i) + "," + str(j) + ",")
-            if str(i) and str(j) in null_dict:
-                f_cos_sim.write(str(cos_sim(null_dict[str(i)], null_dict[str(j)])) + ",")
-                #print(str(i) + "," + str(null_dict[str(i)]))
-                #print(str(i))
-                #f_cos_sim.write(str(i) + "," + str(j) + "," + )
+            # storing node combinations and cosine similarity of null model values.
+            f_cos_sim.write(str(i) + "," + str(j) + "," + str(cos_sim(null_dict[str(i)], null_dict[str(j)])) + ",")
+
+            if (str(i) in real_dict) and (str(j) in real_dict):
+
+                f_cos_sim.write(str(cos_sim(real_dict[str(i)], real_dict[str(j)])) + "," + str(cos_sim(real_dict[str(i)], real_dict[str(j)]) - abs(cos_sim(null_dict[str(i)], null_dict[str(j)]))) + "\n")
+
+            else:
+                f_cos_sim.write("infinite,infinite\n")
 
             j = j + 1
 
@@ -92,60 +99,6 @@ def getting_vector_cosine_sim(vector_file_01, vector_file_02):
     print(end - start)
 
 
-
-
-
-
-def build_word_vector_matrix(vector_file, n_words):
-    #Return the vectors and labels for the first n_words in vector file
-    numpy_arrays = []
-    labels_array = []
-
-    with open(vector_file, 'r') as f:
-        for c, r in enumerate(f):
-            sr = r.split()
-
-            # storing all the labels
-            labels_array.append(sr[0])
-
-            # storing all the vectors
-            numpy_arrays.append(np.array([float(i) for i in sr[1:]]))
-
-
-    labels_array.pop(0)
-    numpy_arrays.pop(0)
-
-    # print(len(numpy_arrays))
-    #print(sorted(labels_array))
-
-    # f_cos_sim = open("nullm_cos_sim.csv", "w")
-    # f_cos_sim.write("node1,node2,cosine_sim\n")
-
-    # As node (i, j) and (j, i) will have same consine similarity, so we are keeping one of them.
-    cos_similarity = []
-
-    start = time.time()
-    # calculating and storing cosine similarity between every node.
-    # for i in range(len(numpy_arrays)):
-    #
-    #     for j in range(len(numpy_arrays)):
-    #
-    #         # removing same pairs of nodes.
-    #         if(labels_array[i] != labels_array[j]):
-    #
-    #             # keeping one pair from (i, j) and (j, i).
-    #             if not (cos_similarity.__contains__(cos_sim(numpy_arrays[i], numpy_arrays[j]))):
-    #
-    #                 f_cos_sim.write(str(labels_array[i]) + "," + str(labels_array[j]) + "," +
-    #                             str(cos_sim(numpy_arrays[i], numpy_arrays[j])) + "\n")
-    #
-    #             cos_similarity.append(cos_sim(numpy_arrays[i], numpy_arrays[j]))
-
-    end = time.time()
-
-    print(end - start)
-
-#build_word_vector_matrix('nodes.emb', 25)
 
 getting_vector_cosine_sim('nodes.emb', 'real_nodes.emb')
 
