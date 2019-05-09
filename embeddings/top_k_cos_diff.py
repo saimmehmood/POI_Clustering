3c8fec0eb94f1f7c26eb60c8d1f8ebcf4869e8b7
saimmehmood <bsef12m557@pucit.edu.pk>
2019-05-08 23:33:56 -0400

    5/8/2019 11:33 PM

--- embeddings/top_k_cos_diff.py
+++ embeddings/top_k_cos_diff.py
@@ -6,7 +6,7 @@ df = pd.read_csv('cos_sim.csv')
 
 diff = df['diff']
 node1 = df['node1']
-node2 = 
+node2 = df['node2']
 
 f = open("top_k.csv", "w")
 
@@ -19,6 +19,7 @@ for i in range(len(diff)):
         cleaned.append(diff[i])
         #f.write(str(diff[i]) + "\n")
 
+# storing cosine similarity differences
 arr = np.array([float(i) for i in cleaned])
 
 k = 10
@@ -26,8 +27,14 @@ k = 10
 top = (arr[np.argsort(arr)[-k:]])
 lowest = (arr[np.argsort(arr)[:k]])
 
+j = 0
 
 
+
+for i in range(len(diff)):
+    if(str(diff[i]) == str(top[j])):
+        print(node1[i], node2[i])
+        j = j + 1
 #end = time.time()
 
 #print(end - start)
