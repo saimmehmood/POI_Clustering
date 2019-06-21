82460302c848a7ba211abcb8fbed6e69ee3dce48
saimmehmood <bsef12m557@pucit.edu.pk>
2019-06-21 12:43:03 -0400

    6/21/2019

--- visualization/plotting_poi.py
+++ visualization/plotting_poi.py
@@ -11,14 +11,14 @@ gmap = gmplot.GoogleMapPlotter(43.739829, -79.514102, 13, API_KEY)
 
 def getData():
 
-	path_to_json = 'C:\\Users\\saim\\Documents\\POI_Clustering\\POI_datasets\\new_north_york'
+	path_to_json = 'C:\\Users\\saim\\Documents\\POI_Clustering\\POI_datasets\\downtown_toronto'
 	json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 	data = []
 	for i in range(len(json_files)):
 		#print(json_files[i])
 		with open(path_to_json + '/' + str(json_files[i])) as file:
 			data = json.load(file)
-		gmap.marker(data["geometry"]["location"]["lat"], data["geometry"]["location"]["lng"], 'cornflowerblue')
+		gmap.marker(data["geometry"]["location"]["lat"], data["geometry"]["location"]["lng"], 'green')
 
 getData()
 gmap.draw("my_map.html")
