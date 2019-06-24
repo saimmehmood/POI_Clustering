e20202b4e51deb9dc30870e67d8f98091549e3d9
saimmehmood <bsef12m557@pucit.edu.pk>
2019-06-24 16:37:23 -0400

    6/24/2019 4:37 PM

--- visualization/plotting_poi.py
+++ visualization/plotting_poi.py
@@ -4,11 +4,13 @@ import os, json
 
 API_KEY = 'AIzaSyCcFSRX4kExONVVB9cqQynjh7EXgZwcyaI'
 
-gmap = gmplot.GoogleMapPlotter(43.739829, -79.514102, 13, API_KEY)
+gmap = gmplot.GoogleMapPlotter(52.517671, 13.377802, 13, API_KEY)
 
 
-path_to_json = 'C:\\Users\\saim\\Documents\\POI_Clustering\\POI_datasets\\downtown_toronto'
+path_to_json = 'C:\\Users\\saim\\Documents\\POI_Clustering\\POI_datasets\\downtown_berlin'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+
+print(len(json_files))
 
 for i in range(len(json_files)):
 	with open(path_to_json + '/' + str(json_files[i])) as file:
